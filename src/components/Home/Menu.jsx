3fad13aa42a5733bf2403e951c398049ca2262c8
Roman Pohribniak <roman.pohribniak@gmail.com>
2022-05-27 10:58:46 +0300

    add menu

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -4,12 +4,22 @@ import { Offcanvas } from "react-bootstrap";
 export default function Menu() {
   return (
     <>
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+      <Offcanvas.Header>
+        <div className="menu__cont">
+          <div className="menu__icon"></div>
+          <div className="menu__cont--info">
+            <div className="menu__name">Roman Pohribniak</div>
+            <div className="menu__status">Online</div>
+          </div>
+        </div>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        Some text as placeholder. In real life you can have the elements you
-        have chosen. Like, text, images, lists, etc.
+        <ul>
+          <li>Contacts</li>
+          <li>Settings</li>
+          <li>Night mode</li>
+          <li>Log out</li>
+        </ul>
       </Offcanvas.Body>
     </>
   );
