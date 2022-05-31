de83a5990dc7e069b6a3702198d6915c9a7f9601
Roman Pohribniak <roman.pohribniak@gmail.com>
2022-05-31 21:00:40 +0300

    add chat input

--- src/components/Home/chat/ChatFooter.jsx
+++ src/components/Home/chat/ChatFooter.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
+import { ImAttachment } from "react-icons/im";
+import { GrEmoji } from "react-icons/gr";
+import { MdOutlineKeyboardVoice } from "react-icons/md";
+import { FiSend } from "react-icons/fi";
+
 export default function ChatFooter() {
-  return <div className="chat__footer">
-      <div className="chat__footer--attach"></div>
-      <div className="chat__footer--input"></div>
-      <div className="chat__footer--emoji"></div>
-      <div className="chat__footer--audio"></div>
-  </div>;
+  const [height, setHeight] = useState(29);
+  const [isInputNull, updateInputStatus] = useState(true);
+
+  return (
+    <div className="chat__footer" style={{ height: `${height}px` }}>
+      <div className="chat__footer--attach">
+        <ImAttachment />
+      </div>
+      <div className="chat__footer--input">
+        <textarea
+          className="chat__footer--message"
+          onInput={(e) => {
+            e.target.style.height = "0px";
+            e.target.style.height = `${e.target.scrollHeight}px`;
+            setHeight(29 + e.target.scrollHeight);
+            if (e.target.value.trim() !== "") {
+              updateInputStatus(false);
+            } else {
+              updateInputStatus(true);
+            }
+          }}
+          onKeyPress={e=>{
+            if(e.key === "Enter" && !e.shiftKey){
+              e.target.value = " "
+            }
+          }}
+          placeholder="Write a message..."
+        />
+      </div>
+      <div className="chat__footer--emoji">
+        <GrEmoji />
+      </div>
+      {isInputNull ? (
+        <div className="chat__footer--audio">
+          <MdOutlineKeyboardVoice />
+        </div>
+      ) : (
+        <div className="chat__footer--sendmsg">
+          <FiSend />
+        </div>
+      )}
+    </div>
+  );
 }
